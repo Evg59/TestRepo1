9a9226db0f03cc24714f976f9604dffefe3fef6b
PIBO0121001 Ольховский Евгений Федорович <evgeniy.olkhovskiy97@gmail.com>
2022-10-08 11:24:40 +0700

    Добавлены файлы проекта

---
+++ ConsoleApp2/Repo 1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+internal class programm
+{
+    static void Main(string[] args)
+    {
+        int a = 0;
+        string str;
+        Console.WriteLine("Напишите год");
+
+        str = Console.ReadLine();
+        a = Convert.ToInt32(str);
+
+        if (a % 4 == 0)
+        {
+
+            Console.WriteLine("Високосный год");
+        }
+        else
+        {
+            Console.WriteLine("Не високосный год");
+        }
+    }
+}
